7d57b9445e5f5715a7f64db767de9f47fefb0e27
Siew Wing Fei <siewwingfei@hotmail.com>
2020-07-17 18:04:34 +0800

    i

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ams',
+    component: IndexComponent
+  },
+  { path: '**', redirectTo: '/ams' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    scrollPositionRestoration: 'top',
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgxUiLoaderModule, NgxUiLoaderHttpModule, POSITION } from 'ngx-ui-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    ModalModule.forRoot(),
+    NgxUiLoaderModule.forRoot({
+      bgsPosition: POSITION.centerCenter,
+      bgsColor: '#007bff',
+      bgsType: 'rotating-plane',
+      fgsColor: '#007bff',
+      fgsType: 'rotating-plane'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
