320be3b2fe5fdff1a59f7a13b4145978af0d4fdd
Siew Wing Fei <siewwingfei@hotmail.com>
2020-07-29 15:27:15 +0800

    i

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  client_id: '9BMd5I89dGwEnyxQk4v9fjIMElRBdRUwF4JINpc5',
-  baseUrl: 'http://localhost:8100'
+  client_id: '0t8yFmNoeqbYjP4hhPjxKMQ5nUeyHjggHdNZb6c2',
+  baseUrl: 'https://mysite-ams.herokuapp.com'
 };
