8783997ca4333cfe47b32af623ec5e7ec725b8dd
Siew Wing Fei <siewwingfei@hotmail.com>
2020-07-30 10:02:20 +0800

    i

--- src/app/shared/utils/helper.ts
+++ src/app/shared/utils/helper.ts
@@ -72,7 +72,7 @@ export class Helper {
 
   public static getPercentage(x, total) {
     let pct = x * 100 / total;
-    pct = Math.trunc(pct);
+    pct = Math.round(pct);
     return `${pct} %`;
   }
 }
