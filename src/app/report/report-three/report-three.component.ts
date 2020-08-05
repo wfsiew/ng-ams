032f963634e0616f8f6c2a1ef77be919dcd1cd75
Siew Wing Fei <siewwingfei@hotmail.com>
2020-08-05 12:45:39 +0800

    i

--- src/app/report/report-three/report-three.component.ts
+++ src/app/report/report-three/report-three.component.ts
@@ -123,6 +123,14 @@ export class ReportThreeComponent implements OnInit {
       else if (this.tab === 1) {
         this.data = this.getData1ByMine();
       }
+
+      else if (this.tab === 2) {
+        this.data = this.getData1ByTonTotal();
+      }
+
+      else if (this.tab === 3) {
+        this.data = this.getData1ByMineTotal();
+      }
     }
     
     else if (this.mine === '1') {
@@ -130,14 +138,30 @@ export class ReportThreeComponent implements OnInit {
         this.data = this.getData2ByTon();
       }
 
-      else {
+      else if (this.tab === 1) {
         this.data = this.getData2ByMine();
+      }
+
+      else if (this.tab === 2) {
+        this.data = this.getData2ByTonTotal();
+      }
+
+      else if (this.tab === 3) {
+        this.data = this.getData2ByMineTotal();
       }
     }
 
     if (this.chart) {
       this.chart.data = this.data;
       this.chart.options.title.text = this.chartTitle;
+
+      if (this.tab === 0 || this.tab === 2) {
+        this.chart.options.scales.yAxes[0].scaleLabel.labelString = 'Tan';
+      }
+
+      else if (this.tab === 1 || this.tab === 3) {
+        this.chart.options.scales.yAxes[0].scaleLabel.labelString = 'Lombong';
+      }
       this.chart.reinit();
     }
   }
@@ -179,6 +203,26 @@ export class ReportThreeComponent implements OnInit {
           s = 'Besi - Jumlah Lombong (Negeri) dari 2014 - 2018';
         }
       }
+
+      else if (this.tab === 2) {
+        if (this.opt === '0') {
+          s = 'Besi - Jumlah Tan dari 2016 - 2018';
+        }
+        
+        else {
+          s = 'Besi - Jumlah Tan dari 2014 - 2018';
+        }
+      }
+
+      else if (this.tab === 3) {
+        if (this.opt === '0') {
+          s = 'Besi - Jumlah Lombong dari 2016 - 2018';
+        }
+        
+        else {
+          s = 'Besi - Jumlah Lombong dari 2014 - 2018';
+        }
+      }
     }
 
     else if (this.mine === '1') {
@@ -199,6 +243,26 @@ export class ReportThreeComponent implements OnInit {
         
         else {
           s = 'Timah - Jumlah Lombong (Negeri) dari 2015 - 2019';
+        }
+      }
+
+      else if (this.tab === 2) {
+        if (this.opt === '0') {
+          s = 'Timah - Jumlah Tan dari 2017 - 2019';
+        }
+        
+        else {
+          s = 'Timah - Jumlah Tan dari 2015 - 2019';
+        }
+      }
+
+      else if (this.tab === 3) {
+        if (this.opt === '0') {
+          s = 'Timah - Jumlah Lombong dari 2017 - 2019';
+        }
+        
+        else {
+          s = 'Timah - Jumlah Lombong dari 2015 - 2019';
         }
       }
     }
@@ -274,6 +338,130 @@ export class ReportThreeComponent implements OnInit {
     }
   }
 
+  private getData1ByTonTotal() {
+    const colorList = Helper.getColorList([1, 2, 3, 4, 5]);
+
+    if (this.opt === '0') {
+      const lx = _.map(data_besi_ton.datasets, (x, i) => {
+        return {
+          data: _.slice(x.data, 2)
+        }
+      });
+      let ls = [];
+      _.each([0, 1, 2], (x, i) => {
+        let a = 0;
+        _.each(lx, (k) => {
+          a += k.data[i];
+        });
+
+        ls.push(a);
+      });
+
+      return {
+        labels: _.slice(data_besi_ton.labels, 2),
+        datasets: [
+          {
+            label: 'Jumlah Tan',
+            data: ls,
+            backgroundColor: '#42A5F5',
+            borderColor: '#42A5F5'
+          }
+        ]
+      };
+    }
+
+    else {
+      const lx = _.map(data_besi_ton.datasets, (x, i) => {
+        return {
+          data: x.data
+        }
+      });
+      let ls = [];
+      _.each([0, 1, 2, 3, 4], (x, i) => {
+        let a = 0;
+        _.each(lx, (k) => {
+          a += k.data[i];
+        });
+
+        ls.push(a);
+      });
+
+      return {
+        labels: data_besi_ton.labels,
+        datasets: [
+          {
+            label: 'Jumlah Tan',
+            data: ls,
+            backgroundColor: '#42A5F5',
+            borderColor: '#42A5F5'
+          }
+        ]
+      };
+    }
+  }
+
+  private getData1ByMineTotal() {
+    const colorList = Helper.getColorList([1, 2, 3, 4, 5]);
+
+    if (this.opt === '0') {
+      const lx = _.map(data_besi_lombong.datasets, (x, i) => {
+        return {
+          data: _.slice(x.data, 2)
+        }
+      });
+      let ls = [];
+      _.each([0, 1, 2], (x, i) => {
+        let a = 0;
+        _.each(lx, (k) => {
+          a += k.data[i];
+        });
+
+        ls.push(a);
+      });
+
+      return {
+        labels: _.slice(data_besi_lombong.labels, 2),
+        datasets: [
+          {
+            label: 'Jumlah Lombong',
+            data: ls,
+            backgroundColor: '#9CCC65',
+            borderColor: '#9CCC65'
+          }
+        ]
+      };
+    }
+
+    else {
+      const lx = _.map(data_besi_lombong.datasets, (x, i) => {
+        return {
+          data: x.data
+        }
+      });
+      let ls = [];
+      _.each([0, 1, 2, 3, 4], (x, i) => {
+        let a = 0;
+        _.each(lx, (k) => {
+          a += k.data[i];
+        });
+
+        ls.push(a);
+      });
+
+      return {
+        labels: data_besi_lombong.labels,
+        datasets: [
+          {
+            label: 'Jumlah Lombong',
+            data: ls,
+            backgroundColor: '#9CCC65',
+            borderColor: '#9CCC65'
+          }
+        ]
+      };
+    }
+  }
+
   private getData2ByTon() {
     const colorList = Helper.getColorList([1, 2, 3, 4]);
 
@@ -338,6 +526,130 @@ export class ReportThreeComponent implements OnInit {
       return {
         labels: data_timah_lombong.labels,
         datasets: lx
+      };
+    }
+  }
+
+  private getData2ByTonTotal() {
+    const colorList = Helper.getColorList([1, 2, 3, 4]);
+
+    if (this.opt === '0') {
+      const lx = _.map(data_timah_ton.datasets, (x, i) => {
+        return {
+          data: _.slice(x.data, 2)
+        }
+      });
+      let ls = [];
+      _.each([0, 1, 2], (x, i) => {
+        let a = 0;
+        _.each(lx, (k) => {
+          a += k.data[i];
+        });
+
+        ls.push(a);
+      });
+
+      return {
+        labels: _.slice(data_timah_ton.labels, 2),
+        datasets: [
+          {
+            label: 'Jumlah Tan',
+            data: ls,
+            backgroundColor: '#42A5F5',
+            borderColor: '$42A5F5'
+          }
+        ]
+      };
+    }
+
+    else {
+      const lx = _.map(data_timah_ton.datasets, (x, i) => {
+        return {
+          data: x.data
+        }
+      });
+      let ls = [];
+      _.each([0, 1, 2, 3, 4], (x, i) => {
+        let a = 0;
+        _.each(lx, (k) => {
+          a += k.data[i];
+        });
+
+        ls.push(a);
+      });
+
+      return {
+        labels: data_timah_ton.labels,
+        datasets: [
+          {
+            label: 'Jumlah Tan',
+            data: ls,
+            backgroundColor: '#42A5F5',
+            borderColor: '#42A5F5'
+          }
+        ]
+      };
+    }
+  }
+
+  private getData2ByMineTotal() {
+    const colorList = Helper.getColorList([1, 2, 3, 4, 5]);
+
+    if (this.opt === '0') {
+      const lx = _.map(data_timah_lombong.datasets, (x, i) => {
+        return {
+          data: _.slice(x.data, 2)
+        }
+      });
+      let ls = [];
+      _.each([0, 1, 2], (x, i) => {
+        let a = 0;
+        _.each(lx, (k) => {
+          a += k.data[i];
+        });
+
+        ls.push(a);
+      });
+
+      return {
+        labels: _.slice(data_timah_lombong.labels, 2),
+        datasets: [
+          {
+            label: 'Jumlah Lombong',
+            data: ls,
+            backgroundColor: '#9CCC65',
+            borderColor: '#9CCC65'
+          }
+        ]
+      };
+    }
+
+    else {
+      const lx = _.map(data_timah_lombong.datasets, (x, i) => {
+        return {
+          data: x.data
+        }
+      });
+      let ls = [];
+      _.each([0, 1, 2, 3, 4], (x, i) => {
+        let a = 0;
+        _.each(lx, (k) => {
+          a += k.data[i];
+        });
+
+        ls.push(a);
+      });
+
+      return {
+        labels: data_timah_lombong.labels,
+        datasets: [
+          {
+            label: 'Jumlah Lombong',
+            data: ls,
+            backgroundColor: '#9CCC65',
+            borderColor: '#9CCC65'
+          }
+        ]
       };
     }
   }
