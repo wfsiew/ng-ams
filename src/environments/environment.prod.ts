9a8bed9ee6051437568c349a159ca5ca03832647
Siew Wing Fei <wingfei.siew@nova-hub.com>
2020-08-12 22:32:02 +0800

    i

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   // client_id: '0t8yFmNoeqbYjP4hhPjxKMQ5nUeyHjggHdNZb6c2',
   // baseUrl: 'https://mysite-ams.herokuapp.com'
   client_id: '9BMd5I89dGwEnyxQk4v9fjIMElRBdRUwF4JINpc5',
-  baseUrl: 'http://10.238.5.32/mysite'
+  baseUrl: 'http://211.24.77.252/mysite'
 };
